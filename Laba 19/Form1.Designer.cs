24e4552ac5d05fffd902fd13103017c1f5176e73
OleksandrBuievych <112573653+OleksandrBuievych@users.noreply.github.com>
2023-04-12 23:42:32 +0300

    Добавьте файлы проекта.

---
+++ Laba 19/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace Laba_19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputTextBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.deleteTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1249, 643);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.outputLabel);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.inputTextBox);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1241, 614);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Завдання 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.deleteTextBox);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.inputTextBox1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1241, 614);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Завдання 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(521, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть текст в якому потрібно знайти однакові слова";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(615, 90);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(609, 22);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(322, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(600, 204);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Знайти однакові слова";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(414, 181);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 17);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(351, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Введіть текст";
+            // 
+            // inputTextBox1
+            // 
+            this.inputTextBox1.Location = new System.Drawing.Point(560, 151);
+            this.inputTextBox1.Name = "inputTextBox1";
+            this.inputTextBox1.Size = new System.Drawing.Size(570, 22);
+            this.inputTextBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(134, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(360, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Введіть слово яке потрібно видалити";
+            // 
+            // deleteTextBox
+            // 
+            this.deleteTextBox.Location = new System.Drawing.Point(560, 220);
+            this.deleteTextBox.Name = "deleteTextBox";
+            this.deleteTextBox.Size = new System.Drawing.Size(570, 22);
+            this.deleteTextBox.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(272, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(611, 157);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Видалити слово з тексту";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1240, 644);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox deleteTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inputTextBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Laba 19/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Перевірити, чи введено рядок для підрахунку слів
+            if (string.IsNullOrWhiteSpace(inputTextBox.Text))
+            {
+                MessageBox.Show("Будь ласка, введіть рядок для підрахунку слів.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Отримати введений рядок та очистити його від зайвих пробілів
+            string inputString = inputTextBox.Text.Trim();
+
+            // Розділити введений рядок на слова та підрахувати кількість кожного слова
+            var wordFrequency = inputString.Split(' ')
+                .GroupBy(word => word, StringComparer.OrdinalIgnoreCase)
+                .ToDictionary(group => group.Key, group => group.Count());
+
+            // Вивести кількість кожного слова у вихідний лейбл
+            outputLabel.Text = "Частота слів:\n";
+            foreach (var pair in wordFrequency)
+            {
+                if (pair.Value > 1)
+                {
+                    outputLabel.Text += $"{pair.Key}: {pair.Value}\n";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Перевірити, чи введено рядок та слово для видалення
+            if (string.IsNullOrWhiteSpace(inputTextBox1.Text))
+            {
+                MessageBox.Show("Будь ласка, введіть рядок для видалення слів.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(deleteTextBox.Text))
+            {
+                MessageBox.Show("Будь ласка, введіть слово для видалення.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Отримати введений рядок та слово для видалення та очистити їх від зайвих пробілів
+            string inputString = inputTextBox1.Text.Trim();
+            var wordToRemove = deleteTextBox.Text.Trim();
+
+            // Видалити всі входження вказаного слова з введеного рядка
+            inputString = string.Join(" ", inputString.Split(' ')
+                .Where(word => !string.Equals(word, wordToRemove, StringComparison.OrdinalIgnoreCase)));
+
+            // Вивести змінений рядок у вікно
+            MessageBox.Show($"{inputString}", "Відформатований текст");
+        }
+    }
+}
+
